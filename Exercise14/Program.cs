107429e9ea18de93b2f3505d07197cda066bc345
Your Name <kennystemper@gmail.com>
2021-04-10 15:30:10 -0400

    Exercise 14

---
+++ Exercise14/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Exercise14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercise 14");
+            bool repeat = true;
+            while (repeat)
+            {
+                //Prompt the user for input
+                Console.WriteLine("Please enter a number greater than 0");
+                string input = Console.ReadLine();
+                int number = int.Parse(input);
+
+                //The square if i will be produced until i equals the number input by the user
+                for (int i = 1; number >= i; i++)
+                {
+                    int square = i * i;
+                    Console.WriteLine(square);
+                }
+                //prompt the user for additional input
+                Console.WriteLine("Enter another number? y/n");
+                string answer = Console.ReadLine();
+
+                if (answer == "y")
+                {
+                    //An answer if "y" will repeat the program
+                    repeat = true;
+                }
+                else
+                {
+                    //Any answer other than "y" will terminate the program
+                    Console.WriteLine("Goodbye!");
+                    repeat = false;
+                }
+            }
+        }
+    }
+}
